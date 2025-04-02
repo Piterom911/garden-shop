4513c20e49878fcbd1f3acf342567cd4bbcfaa9b
Roman Dogoda <piterom911@gmail.com>
2025-04-02 11:14:38 +0200

    Garden Shop:
    Base construction: pom.xml, resources.db, application.properties, GardenShopApp.

---
+++ src/main/java/com/predators/GardenShopApp.java
@@ -0,0 +1,12 @@
+package com.predators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenShopApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GardenShopApp.class, args);
+    }
+}

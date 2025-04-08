0858b3cf70d425f73df16d7aae14695a4e34d527
Kostiantyn19899 <sierikov.de@gmail.cim>
2025-04-08 20:02:30 +0200

    Garden Shop
    add Cart Table

---
+++ src/main/java/com/predators/entity/Cart.java
@@ -0,0 +1,28 @@
+package com.predators.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "carts")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @OneToMany(mappedBy = "cartId")
+    private List<CartItem> cartItems;
+}

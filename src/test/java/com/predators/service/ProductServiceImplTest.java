ff5f395fbf037b82c015da295759c79830d1ccc4
Kostiantyn19899 <sierikov.de@gmail.cim>
2025-04-09 15:00:35 +0200

    Garden Shop
    fix ProductServiceImplTest

--- src/test/java/com/predators/service/ProductServiceImplTest.java
+++ src/test/java/com/predators/service/ProductServiceImplTest.java
@@ -1,5 +1,8 @@
 package com.predators.service;
 
+import com.predators.dto.converter.ProductConverter;
+import com.predators.dto.product.ProductRequestDto;
+import com.predators.entity.Category;
 import com.predators.entity.Product;
 import com.predators.exception.ProductNotFoundException;
 import com.predators.repository.ProductJpaRepository;
@@ -8,9 +11,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -19,18 +24,33 @@ class ProductServiceImplTest {
     @Mock
     private ProductJpaRepository repository;
 
+    @Mock
+    private CategoryService categoryService;
+
     @InjectMocks
-    private ProductServiceImpl service;
+    private ProductServiceImpl productService;
+
+    @InjectMocks
+    private ProductConverter converter;
+
 
     @Test
     public void testGetAll() {
-        service.getAll();
+        productService.getAll();
         verify(repository, times(1)).findAll();
     }
 
+
     @Test
     void testCreate() {
-        service.create(new Product());
+        Category mockCategory = new Category();
+        mockCategory.setId(1L);
+
+        when(categoryService.getById(1L)).thenReturn(mockCategory);
+
+        Product entity = converter.toEntity(new ProductRequestDto("banana", "banana",
+                new BigDecimal(15), 1L, "http/url"));
+        productService.create(entity);
         verify(repository, times(1)).save(any(Product.class));
     }
 
@@ -40,7 +60,7 @@ class ProductServiceImplTest {
         Product expectedProduct = new Product();
         when(repository.findById(id)).thenReturn(Optional.of(expectedProduct));
 
-        Product actualProduct = service.getById(id);
+        Product actualProduct = productService.getById(id);
 
         assertEquals(expectedProduct, actualProduct);
         verify(repository, times(1)).findById(id);
@@ -49,12 +69,12 @@ class ProductServiceImplTest {
     @Test
     void testGetByIdNotFound() {
         assertThrows(ProductNotFoundException.class, () ->
-                service.getById(2L));
+                productService.getById(2L));
     }
 
     @Test
     void delete() {
-        service.delete(1L);
+        productService.delete(1L);
         verify(repository, times(1)).deleteById(1L);
     }
 }

f3437bc71e0f7707d94d654a6c690311d46905a7
Kostiantyn19899 <sierikov.de@gmail.cim>
2025-05-15 09:29:27 +0200

    Garden Shop
    I did the tests, everything works.

--- src/main/java/com/predators/dto/product/ProductFilterDto.java
+++ src/main/java/com/predators/dto/product/ProductFilterDto.java
@@ -1,7 +1,10 @@
 package com.predators.dto.product;
 
+import lombok.Builder;
+
 import java.math.BigDecimal;
 
+@Builder
 public record ProductFilterDto (BigDecimal minPrice,
                                 BigDecimal maxPrice,
                                 Boolean discount,
